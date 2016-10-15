3b47bceea6385f32ac18a2084cbbdf184926747f
chris ertel <chris@kedagital.com>
2016-10-14 23:41:12 -0500

    Add makefile support.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule ElixirGame.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     compilers: [:elixir_make] ++ Mix.compilers,
      deps: deps()]
   end
 
@@ -29,5 +30,6 @@ defmodule ElixirGame.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
    # [{:base, path: "../base"}]
+   [{:elixir_make, "~> 0.3.0"}]
   end
 end
